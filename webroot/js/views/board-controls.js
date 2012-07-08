90f267edd9711a53f9085d7ce0e777b3e995d8d0
Christian Siegert <christian@christian-siegert.com>
2012-07-08 22:18:23 +0200

    Added doc-blocks.

--- webroot/js/views/board-controls.js
+++ webroot/js/views/board-controls.js
@@ -1,10 +1,18 @@
 (function() {
 	var exports = app.namespace("app.views.boardControls");
 
+	/**
+	 * createInstance returns a new instance of class BoardControls.
+	 * @returns object BoardControls
+	 */
 	exports.createInstance = function() {
 		return new BoardControls();
 	};
 
+	/**
+	 * Class BoardControls manages an anchor element that users click on to load
+	 * more images.
+	 */
 	function BoardControls() {
 		var self = this;
 
