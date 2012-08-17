e229b01cd11202ca01e6c1e77de46b939380ba4f
Christian Siegert <christian@christian-siegert.com>
2012-08-17 12:52:55 +0200

    Changed 'Load images' link to button. Added CSS to style button.

--- webroot/js/views/board-controls.js
+++ webroot/js/views/board-controls.js
@@ -10,8 +10,15 @@
 	};
 
 	/**
-	 * Class BoardControls manages an anchor element that users click on to load
+	 * Class BoardControls manages a button element that users click on to load
 	 * more images.
+	 *
+	 * When we create the button, we give it the text "Loading...", even though
+	 * no images are loading yet, to reduce the perceived page load time.
+	 *
+	 * When users click on the button, we immediately change the button text
+	 * from "Load more" to "Loading...", even though no requests have been sent
+	 * yet, to increase the perceived responsiveness.
 	 */
 	function BoardControls() {
 		var self = this;
@@ -20,37 +27,49 @@
 		 * @var HTMLElement
 		 */
 		var boardControls;
+		var loadMoreButton;
 
 		/**
 		 * @var string
 		 */
-		var loadMoreAnchorTextDefault1 = "Load images";
-		var loadMoreAnchorTextDefault2 = "Load more";
-		var loadMoreAnchorTextActive = "Loading images&hellip;"
+		var loadMoreButtonTextDefault1 = "Load images";
+		var loadMoreButtonTextDefault2 = "Load more";
+		var loadMoreButtonTextActive = "Loading images&hellip;"
 
 		/**
-		 * Flag that indicates if at least one image has been loaded.
+		 * Flag that indicates if at least one image has been loaded. Used to
+		 * determine appropriate button text.
 		 * @var boolean
 		 */
 		var hasLoadedAnImage = false;
 
 		self.create = function() {
-			boardControls = new Element("a", {
-				html: loadMoreAnchorTextDefault1,
+			boardControls = new Element("div", {
 				id: "board-controls"
 			});
 
-			window.addEvent("app.views.board.didCompleteRequest", handleBoardDidCompleteRequestEvent);
+			loadMoreButton = createLoadMoreButton();
+			boardControls.grab(loadMoreButton);
 
-			boardControls.addEvent("click", handleClickEvent);
+			window.addEvent("app.views.board.didCompleteRequest", updateLoadMoreButtonText);
 			return boardControls;
 		};
 
-		function handleClickEvent() {
+		function createLoadMoreButton() {
+			var button = new Element("button", {
+				html: loadMoreButtonTextActive
+			});
+
+			button.addEvent("click", handleLoadMoreButtonClickEvent);
+			return button;
+		}
+
+		function handleLoadMoreButtonClickEvent() {
+			loadMoreButton.set("html", loadMoreButtonTextActive);
 			window.fireEvent("app.views.boardControls.userDidAskForImages");
 		};
 
-		function handleBoardDidCompleteRequestEvent(event) {
+		function updateLoadMoreButtonText(event) {
 			if (!event
 					|| typeof(event.runningRequestsCount) !== "number"
 					|| isNaN(event.runningRequestsCount)) {
@@ -62,16 +81,16 @@
 			}
 
 			if (event.runningRequestsCount > 0) {
-				boardControls.set("html", loadMoreAnchorTextActive);
+				loadMoreButton.set("html", loadMoreButtonTextActive);
 				return;
 			}
 
 			if (hasLoadedAnImage) {
-				boardControls.set("html", loadMoreAnchorTextDefault2);
+				loadMoreButton.set("html", loadMoreButtonTextDefault2);
 				return;
 			}
 
-			boardControls.set("html", loadMoreAnchorTextDefault1);
+			loadMoreButton.set("html", loadMoreButtonTextDefault1);
 		}
 	}
 })();
