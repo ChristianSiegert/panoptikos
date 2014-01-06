06e153a1a44bf4ef3a0a11939c39c7ecee26bb93
Christian Siegert <christian@christian-siegert.com>
2014-01-07 00:18:05 +0100

    Removed some subreddits from list of default subreddits because they are barely active.
    Content of those barely active subreddits rarely changed so we always
    so the same stuff from them.

--- app/webroot/dev-js/services/constants.js
+++ app/webroot/dev-js/services/constants.js
@@ -1,11 +1,8 @@
 app.constant("defaultSubredditIds", [
-	"1000words",
-	"beachporn",
 	"birdpics",
 	"cityporn",
 	"earthporn",
 	"eyecandy",
-	"joshuatree",
 	"lakeporn",
 	"wallpaper",
 	"wallpapers",
