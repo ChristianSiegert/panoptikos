32c14c7ada81f4318c1420143c5074fb0e540759
Christian Siegert <christian@christian-siegert.com>
2014-01-09 18:21:42 +0100

    Improved handling of legacy URLs.

--- app/main.go
+++ app/main.go
@@ -1,3 +1,33 @@
-// Package main is empty, it only exists so the Google App Engine app uploader
-// allows the app to be uploaded.
+// Package main only handles some legacy URLs.
 package main
+
+import (
+	"net/http"
+	"regexp"
+)
+
+func init() {
+	http.HandleFunc("/", handleRequest)
+	http.HandleFunc("/pictures/browse/sfw/", handlePicturesRequest)
+}
+
+// handleRequest treats all incoming requests as requests to legacy URLs and
+// redirects to the website root page.
+func handleRequest(responseWriter http.ResponseWriter, request *http.Request) {
+	http.Redirect(responseWriter, request, "/", http.StatusMovedPermanently)
+}
+
+// handlePicturesRequest redirects from the legacy URL for viewing subreddits to
+// the new URL for viewing subreddits.
+func handlePicturesRequest(responseWriter http.ResponseWriter, request *http.Request) {
+	legacyUrlExpression := regexp.MustCompile("^/pictures/browse/n?sfw/source:reddit-([a-zA-Z0-9_-]+)")
+	matches := legacyUrlExpression.FindStringSubmatch(request.URL.Path)
+
+	if len(matches) == 0 {
+		http.Redirect(responseWriter, request, "/", http.StatusMovedPermanently)
+		return
+	}
+
+	newUrl := "/r/" + matches[1]
+	http.Redirect(responseWriter, request, newUrl, http.StatusMovedPermanently)
+}
