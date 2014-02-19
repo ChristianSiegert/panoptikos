c5cebb2e2f18dabdd7ca3c6af4077e05e6e87f9b
Christian Siegert <christian@christian-siegert.com>
2014-02-19 18:57:36 +0100

    Package “sanitizer”: Improved removal of whitespace in HTML text.

--- assetcompiler/sanitizer/sanitizer.go
+++ assetcompiler/sanitizer/sanitizer.go
@@ -2,30 +2,51 @@
 // templates.
 package sanitizer
 
-import "regexp"
+import (
+	"bytes"
+	"regexp"
+)
 
 var (
-	htmlCommentRegExp             = regexp.MustCompile("<!--.*?-->")
-	whitespaceAtStart             = regexp.MustCompile("^[ \f\n\r\t]+")
-	whitespaceAtEnd               = regexp.MustCompile("[ \f\n\r\t]+$")
-	whitespaceBetweenTags         = regexp.MustCompile(">[ \f\n\r\t]+<")
-	whitespaceBetweenActions      = regexp.MustCompile("}}[ \f\n\r\t]+{{")
-	whitespaceBetweenTagAndAction = regexp.MustCompile(">[ \f\n\r\t]+{{")
-	whitespaceBetweenActionAndTag = regexp.MustCompile("}}[ \f\n\r\t]+<")
+	htmlCommentRegExp             = regexp.MustCompile("<!--(.|[\r\n])*?-->")
+	lineBreakRegExp               = regexp.MustCompile("[\r\n]+")
+	tagRegExp                     = regexp.MustCompile("<(.|[\r\n])*?>")
+	whitespaceAtStart             = regexp.MustCompile("^[ \f\n\r\t\v]+")
+	whitespaceAtEnd               = regexp.MustCompile("[ \f\n\r\t\v]+$")
+	whitespaceBetweenTags         = regexp.MustCompile(">[ \f\n\r\t\v]+<")
+	whitespaceBetweenActions      = regexp.MustCompile("}}[ \f\n\r\t\v]+{{")
+	whitespaceBetweenTagAndAction = regexp.MustCompile(">[ \f\n\r\t\v]+{{")
+	whitespaceBetweenActionAndTag = regexp.MustCompile("}}[ \f\n\r\t\v]+<")
+	whitespaceInsideTagStart      = regexp.MustCompile("<(/)?[ \f\n\r\t\v]+")
+	whitespaceInsideTagEnd        = regexp.MustCompile("[ \f\n\r\t\v]+(/?)>")
+	whitespaceInsideTagEqualSign  = regexp.MustCompile("[ \f\n\r\t\v]*=[ \f\n\r\t\v]*")
+	whitespaceInsideTag           = regexp.MustCompile("[ \f\n\r\t\v]{2,}")
 )
 
 func RemoveHtmlComments(html []byte) []byte {
 	return htmlCommentRegExp.ReplaceAll(html, []byte(""))
 }
 
-// RemoveWhitespace removes whitespace between tags, actions, and at the
+// RemoveHtmlWhitespace removes whitespace between tags, actions, and at the
 // beginning and end of the HTML code.
-func RemoveWhitespace(text []byte) []byte {
-	text = whitespaceBetweenTags.ReplaceAll(text, []byte("><"))
-	text = whitespaceBetweenActions.ReplaceAll(text, []byte("}}{{"))
-	text = whitespaceBetweenTagAndAction.ReplaceAll(text, []byte(">{{"))
-	text = whitespaceBetweenActionAndTag.ReplaceAll(text, []byte("}}<"))
-	text = whitespaceAtStart.ReplaceAll(text, []byte(""))
-	text = whitespaceAtEnd.ReplaceAll(text, []byte(""))
-	return text
+func RemoveHtmlWhitespace(html []byte) []byte {
+	html = whitespaceBetweenTags.ReplaceAll(html, []byte("><"))
+	html = whitespaceBetweenActions.ReplaceAll(html, []byte("}}{{"))
+	html = whitespaceBetweenTagAndAction.ReplaceAll(html, []byte(">{{"))
+	html = whitespaceBetweenActionAndTag.ReplaceAll(html, []byte("}}<"))
+	html = whitespaceAtStart.ReplaceAll(html, []byte(""))
+	html = whitespaceAtEnd.ReplaceAll(html, []byte(""))
+
+	tags := tagRegExp.FindAll(html, -1)
+
+	for _, tag := range tags {
+		cleanTag := lineBreakRegExp.ReplaceAll(tag, []byte(" "))
+		cleanTag = whitespaceInsideTagStart.ReplaceAll(cleanTag, []byte("<$1"))
+		cleanTag = whitespaceInsideTagEnd.ReplaceAll(cleanTag, []byte("$1>"))
+		cleanTag = whitespaceInsideTagEqualSign.ReplaceAllLiteral(cleanTag, []byte("="))
+		cleanTag = whitespaceInsideTag.ReplaceAllLiteral(cleanTag, []byte(" "))
+		html = bytes.Replace(html, tag, cleanTag, 1)
+	}
+
+	return html
 }
--- assetcompiler/sanitizer/sanitizer_test.go
+++ assetcompiler/sanitizer/sanitizer_test.go
@@ -31,6 +31,15 @@ var html = []byte(`
 			<script src="/{{.CompiledJsFile}}"></script>
 		{{end}}
 
+		< div
+			foo
+			bar = "baz"
+			baz1  baz2    baz3
+		>
+		</ div >
+
+		< br >
+
 		<!-- Comment 1 -->
 		<script>var s = "Some JavaScript code"</script>
 
@@ -43,28 +52,28 @@ var html = []byte(`
 `)
 
 func TestRemoveHtmlComments(t *testing.T) {
-	html := []byte(`foo<!-- Comment 1 -->bar<!-- Comment 2 -->baz`)
-	expectedResult := []byte(`foobarbaz`)
+	input := []byte("foo<!-- Comment with newline \n-->bar<!--\n Comment with two newlines \n-->baz")
+	expectedOutput := []byte("foobarbaz")
 
-	result := RemoveHtmlComments(html)
+	result := RemoveHtmlComments(input)
 
-	if len(result) != len(expectedResult) {
+	if len(result) != len(expectedOutput) {
 		t.Errorf("HTML comments weren’t removed correctly: '%s'", result)
 		return
 	}
 
 	for i := range result {
-		if result[i] != expectedResult[i] {
+		if result[i] != expectedOutput[i] {
 			t.Errorf("HTML comments weren’t removed correctly: '%s'", result)
 			return
 		}
 	}
 }
 
-func TestRemoveWhitespace(t *testing.T) {
-	expectedResult := []byte(`<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="initial-scale=1, width=device-width"><title>Panoptikos</title>{{if .IsDevAppServer}}{{range .DevCssFiles}}<link href="{{.}}" rel="stylesheet" type="text/css">{{end}}{{else}}<link href="/{{.CompiledCssFile}}" rel="stylesheet" type="text/css">{{end}}</head><body><p id="some-class">Foo</p><p id="some-other-class">Bar</p>{{if .IsDevAppServer}}{{range .DevJsFiles}}<script src="{{.}}"></script>{{end}}{{else}}<script src="/{{.CompiledJsFile}}"></script>{{end}}<!-- Comment 1 --><script>var s = "Some JavaScript code"</script><!-- Comment 2 --><noscript><div>Enable JavaScript.</div></noscript></body></html>`)
+func TestRemoveHtmlWhitespace(t *testing.T) {
+	expectedResult := []byte(`<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="initial-scale=1, width=device-width"><title>Panoptikos</title>{{if .IsDevAppServer}}{{range .DevCssFiles}}<link href="{{.}}" rel="stylesheet" type="text/css">{{end}}{{else}}<link href="/{{.CompiledCssFile}}" rel="stylesheet" type="text/css">{{end}}</head><body><p id="some-class">Foo</p><p id="some-other-class">Bar</p>{{if .IsDevAppServer}}{{range .DevJsFiles}}<script src="{{.}}"></script>{{end}}{{else}}<script src="/{{.CompiledJsFile}}"></script>{{end}}<div foo bar="baz" baz1 baz2 baz3></div><br><!-- Comment 1 --><script>var s = "Some JavaScript code"</script><!-- Comment 2 --><noscript><div>Enable JavaScript.</div></noscript></body></html>`)
 
-	result := RemoveWhitespace(html)
+	result := RemoveHtmlWhitespace(html)
 
 	if len(result) != len(expectedResult) {
 		t.Errorf("Whitespace wasn’t removed correctly: '%s'", result)
@@ -79,8 +88,8 @@ func TestRemoveWhitespace(t *testing.T) {
 	}
 }
 
-func BenchmarkRemoveWhitespace(b *testing.B) {
+func BenchmarkRemoveHtmlWhitespace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		RemoveWhitespace(html)
+		RemoveHtmlWhitespace(html)
 	}
 }
