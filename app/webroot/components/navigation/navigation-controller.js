12d579b0433ef6fb33e8c3a08d66f74a2d21800c
Christian Siegert <christian@christian-siegert.com>
2014-03-11 18:22:37 +0100

    Fixed problem where navigation bar didn’t show the active link.

--- app/webroot/components/navigation/navigation-controller.js
+++ app/webroot/components/navigation/navigation-controller.js
@@ -1,12 +1,12 @@
 app.controller("n", ["$route", "$scope", function($route, $scope) {
 	$scope.$on("$routeChangeSuccess", function() {
-		$scope.page = {};
+		$scope.p = {};
 
 		if (!$route.current || !$route.current.controller) {
 			return;
 		}
 
 		var controllerName = $route.current.controller.replace(/Controller$/, "");
-		$scope.page[controllerName] = true;
+		$scope.p[controllerName] = true;
 	});
 }]);
